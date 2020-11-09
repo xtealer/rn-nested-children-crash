c62abbf246df5f1048e64abf3f70ac24223fa0be
Enrique Shunnar <xtealer00@gmail.com>
2020-11-09 13:35:35 -0500

    Downgrade to react-native@0.61.2

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image} from 'react-native';
 
 // 10 nested views plus a child
 const TestComponent = ({children}) => {
@@ -48,16 +48,23 @@ const App = () => {
                     <TestComponent>
                       <TestComponent>
                         <TestComponent>
-                          {/*
-                           * 111 tree child item
-                           */}
-                          <View>
-                            {/*
-                             * 112 tree child item
-                             * when commented crash does not happen
-                             */}
-                            <View />
-                          </View>
+                                {/*
+                                 * 111 tree child item
+                                 */}
+                                <Image source={require('./synchronize.png')} />
+                                <View>
+                                  {/*
+                                   * 112 tree child item
+                                   * when commented crash does not happen
+                                   */}
+                                  <View
+                                    style={{
+                                      backgroundColor: 'red',
+                                      height: 50,
+                                      width: 50,
+                                    }}
+                                  />
+                                </View>
                         </TestComponent>
                       </TestComponent>
                     </TestComponent>
